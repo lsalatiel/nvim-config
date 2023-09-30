a1cfcae654ed23cfc687b031ba68e7a040597bdf
lsalatiel <lucassalatiel1004@gmail.com>
2023-09-29 21:05:55 -0300

    update harpoon keymap

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -12,9 +12,9 @@ return {
         keymap.set("n", "<leader>a", mark.add_file)
         keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-        keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-        keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-        keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-        keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+        keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
+        keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
+        keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
+        keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
     end,
 }
