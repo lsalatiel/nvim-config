e9080205edb738f2a2c1cd681cfb4c21cacf11eb
lsalatiel <lucassalatiel1004@gmail.com>
2023-11-28 00:57:40 -0300

    update keybinds

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -9,12 +9,12 @@ return {
         local ui = require("harpoon.ui")
         local keymap = vim.keymap
 
-        keymap.set("n", "<leader>a", mark.add_file)
+        keymap.set("n", "<C-m>", mark.add_file)
         keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-        keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
-        keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
-        keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
-        keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
+        keymap.set("n", "<C-w>", function() ui.nav_file(1) end)
+        keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
+        keymap.set("n", "<C-i>", function() ui.nav_file(3) end)
+        keymap.set("n", "<C-o>", function() ui.nav_file(4) end)
     end,
 }
