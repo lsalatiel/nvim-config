a95bae638730364b1f68b2d6c8256605cf54d5d4
lsalatiel <lucassalatiel1004@gmail.com>
2023-12-01 02:06:48 -0300

    add oil

---
+++ lua/user/plugins/oil.lua
@@ -0,0 +1,7 @@
+return {
+    "stevearc/oil.nvim",
+    config = function ()
+        require("oil").setup({})
+    end,
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+}
