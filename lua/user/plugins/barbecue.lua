9873e2dc46e3cf7d50bb1f35b6b6446b98dc8573
lsalatiel <lucassalatiel1004@gmail.com>
2023-12-01 02:06:23 -0300

    add barbecue

---
+++ lua/user/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+        "smiteshp/nvim-navic",
+        "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+        -- configurations go here
+    },
+}
