f4587430580fe5bb7dabd4fdf69b9ba3c23d9fdf
lsalatiel <lucassalatiel1004@gmail.com>
2023-12-07 13:38:45 -0300

    update lspconfig

--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -20,7 +20,7 @@ return {
 
             -- set keybinds
             opts.desc = "Show LSP references"
-            keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+            keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
             opts.desc = "Go to declaration"
             keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
@@ -74,6 +74,10 @@ return {
         lspconfig["clangd"].setup({
             capabilities = capabilities,
             on_attach = on_attach,
+            cmd = {
+                "clangd",
+                "--offset-encoding=utf-16",
+            },
         })
 
         -- configure python server
